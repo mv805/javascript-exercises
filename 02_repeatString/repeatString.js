e0a9af3e3ef3914254adca226fba586673497549
Matt Villa <matthewvilla805@gmail.com>
2022-04-05 23:31:48 -0700

    Solve 01 and 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (word, repetitions) {
 
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
+
+    let returnString = [];
+
+    for (let i = 1; i <= repetitions; i++) {
+        returnString.push(word);
+    }
+
+    return returnString.join('');
 };
 
 // Do not edit below this line
