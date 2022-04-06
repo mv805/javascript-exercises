fe4ec5f847db59130e72cfefeac2f728a61c688e
Matt Villa <matthewvilla805@gmail.com>
2022-04-05 23:47:14 -0700

    Solve 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let reversedWord = [];
 
+    for (let char of word) {
+        reversedWord.unshift(char);
+    }
+
+    return reversedWord.join('');
 };
 
 // Do not edit below this line
