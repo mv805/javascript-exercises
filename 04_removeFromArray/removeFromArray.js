b3967086cb54e30ef2cf24ba18bfe0bd7ecb05c1
Matt Villa <matthewvilla805@gmail.com>
2022-04-07 01:16:09 -0700

    Solve 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
 
+    let returnArray = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {//loop through arguments to remove 
+        for (let j = 0; j < returnArray.length; j++) {
+            if (returnArray[j] === arguments[i]) {
+                returnArray.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return returnArray;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function () {
+    let num1 = arguments[0];
+    let num2 = arguments[1];
 
+    if (
+        (num1 < 0 || num2 < 0) ||
+        (typeof num2 !== 'number' || typeof num1 !== 'number')
+    ) {
+        return 'ERROR';
+    }
+
+    let firstIndex;
+    let lastIndex;
+
+    if (num1 > num2) {
+        firstIndex = num2;
+        lastIndex = num1;
+    } else {
+        firstIndex = num1;
+        lastIndex = num2
+    }
+    let finalSum = 0;
+
+    for (i = firstIndex; i <= lastIndex; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
